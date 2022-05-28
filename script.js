798a9c4d3e0b248d8ba32de393bbfc11e4f7ac6c
diksha <diksha269>
2022-05-28 17:57:28 +0530

    Done!

---
+++ script.js
@@ -0,0 +1,34 @@
+// Shortcuts simplify My Drive … 
+// Over the coming weeks, items in more than one folder will be replaced by shortcuts. Access to files and folders won't change.Learn more
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
